5a7bb3642be357f44218a004a00993ae99fa141d
Suren Mohandass <suren.mohandass@microsoft.com>
2020-05-26 12:59:42 +0100

    Added steps for setup

--- strategies/canary/01-kubernetes-native/scripts/01-setup.sh
+++ strategies/canary/01-kubernetes-native/scripts/01-setup.sh
@@ -1,7 +1,39 @@
+#!/bin/bash
+
+instance_count=$1
+namespace="canary-native"
+
 # accept parameter for number of instances (default 10)
+if [[ -z "$1" ]]; # or [ $# -eq 0 ]
+  then        
+    read -p "Required Number of Instances (default is 10): "
+    instance_count=$REPLY
+fi
 
 # delete namespace if exists
+echo "...deleting $namespace namespace if it already exists.."
+kubectl delete ns $namespace
 
 # create namesapce
+echo "...creating $namespace namespace.."
+kubectl create ns $namespace
 
 # deploy manifests
+echo "...creating deployment for deployment.yaml"
+kubectl apply -f ../manifests/deployment.yaml -n $namespace
+
+if [[ -z "$instance_count" ]]
+then
+  echo "...setting replicas to default instance count of 10"
+else
+  echo "...scaling deployment to $instance_count"
+  kubectl scale --replicas=$instance_count deployment/myapp -n $namespace
+fi
+
+echo "...creating a service"
+kubectl apply -f ../manifests/service.yaml -n $namespace
+
+echo "...creating a service monitor"
+kubectl apply -f ../manifests/servicemonitor.yaml
+
+echo "...done"
