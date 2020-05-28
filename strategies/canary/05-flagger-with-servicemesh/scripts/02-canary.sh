0fcb7cc460fc74cc802ac13ae1c0127611d193b8
Suren Mohandass <Suren.Mohandass@microsoft.com>
2020-05-28 23:13:12 +0000

    fixed namespace

--- strategies/canary/05-flagger-with-servicemesh/scripts/02-canary.sh
+++ strategies/canary/05-flagger-with-servicemesh/scripts/02-canary.sh
@@ -5,8 +5,8 @@ namespace=$1
 
 if [[ -z "$1" ]]; 
   then        
-    read -p "Target Namespace (default is canary-flagger) : "
-    namespace=${REPLY:-"canary-flagger"}
+    read -p "Target Namespace (default is canary-mesh) : "
+    namespace=${REPLY:-"canary-mesh"}
 fi
 
 printf "\n...creating canary resource for the app\n"
