1753e73e5c35bbfb1f59b7d60ca5fb615115d8ea
Liam Moat <liam.moat@microsoft.com>
2020-06-08 14:00:49 +0100

    remove reject script

--- strategies/canary/02-kubernetes-with-helm/scripts/04-reject.sh
+++
@@ -1,9 +0,0 @@
-# ./scripts/04-reject.sh canary-demo-02
-
-NAMESPACE="canary-helm"
-
-# rollback canary deployment
-helm rollback my-release -n $NAMESPACE
-
-# get all resources
-kubectl get all -n $NAMESPACE     
